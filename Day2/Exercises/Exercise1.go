0ee8b458a4cad66532a3727680d13658f611e030
zahlekhan <zahle.khan@razorpay.com>
2021-07-23 06:35:09 +0530

    patch error which occurs when there are more than one WordToLettersWorkers
    This error is because each WordToLetters worker closes incrementChan after exiting
    Code was refactored to use waitGroup and channel close was done in the main goroutine

--- Day2/Exercises/Exercise1.go
+++ Day2/Exercises/Exercise1.go
@@ -20,8 +20,8 @@ func Counter(letterFrequency []uint64, incrementChan chan Increment, wg *sync.Wa
 	}
 }
 
-func WordToLetters(wordChan chan string, incrementChan chan Increment) {
-	defer close(incrementChan)
+func WordToLetters(wordChan chan string, incrementChan chan Increment, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for word := range wordChan {
 		for _, letter := range word {
 			newIncrement := Increment{int(unicode.ToLower(letter) - 'a'), 1}
@@ -33,26 +33,32 @@ func WordToLetters(wordChan chan string, incrementChan chan Increment) {
 func CountLetterFrequency(Words ...string) map[string]uint64 {
 
 	letterFrequency := make([]uint64, 26)
-	wg := sync.WaitGroup{}
+	CounterWG := sync.WaitGroup{}
 	incrementChan := make(chan Increment)
 
 	CounterWorkers := 2
 	for i := 1; i <= CounterWorkers; i++ {
-		wg.Add(1)
-		go Counter(letterFrequency, incrementChan, &wg)
+		CounterWG.Add(1)
+		go Counter(letterFrequency, incrementChan, &CounterWG)
 	}
 
+	WordToLettersWG := sync.WaitGroup{}
 	wordChan := make(chan string)
-	WordToLettersWorkers := 1
+
+	WordToLettersWorkers := 3
 	for i := 1; i <= WordToLettersWorkers; i++ {
-		go WordToLetters(wordChan, incrementChan)
+		WordToLettersWG.Add(1)
+		go WordToLetters(wordChan, incrementChan, &WordToLettersWG)
 	}
 
 	for _, word := range Words {
 		wordChan <- word
 	}
 	close(wordChan)
-	wg.Wait()
+	WordToLettersWG.Wait()
+
+	close(incrementChan)
+	CounterWG.Wait()
 
 	return GenerateFrequency(letterFrequency)
 }
